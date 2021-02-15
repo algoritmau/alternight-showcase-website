aa7152dabfcb69c224357addfdbe66eff51c5427
Mauricio Paternina <spaceinvadev@gmail.com>
2021-02-15 09:29:30 -0500

    add product hunt badge

---
+++ main.js
@@ -0,0 +1,22 @@
+const productHuntBadge = document.querySelector('.product-hunt-badge');
+
+const getScrollHeight = () =>
+  Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.body.clientHeight,
+    document.documentElement.clientHeight
+  );
+
+window.addEventListener('scroll', () => {
+  let currentScrollHeight = getScrollHeight();
+
+  if (
+    window.pageYOffset >= currentScrollHeight * 0.7 &&
+    !productHuntBadge.hidden
+  ) {
+    productHuntBadge.hidden = true;
+  }
+});
